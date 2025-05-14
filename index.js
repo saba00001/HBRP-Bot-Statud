885e2aa4abe7f094feb0a3ed11893b0e522985b0
saba00001 <164893567+saba00001@users.noreply.github.com>
2025-05-14 17:22:50 +0400

    Update index.js

--- index.js
+++ index.js
@@ -152,9 +152,9 @@ client.on('messageCreate', async (message) => {
       return message.reply(`⚠️ არასწორი ფერის HEX კოდი. გამოიყენეთ ფორმატი #RRGGBB ან #RGB (მაგ: #FF0000 წითელისთვის, #00FF00 მწვანისთვის)`);
     }
     
-    // შექმენით embed შეტყობინება ფერადი ზოლით და მუქი ტექსტით (bold)
+    // შექმენით embed შეტყობინება ფერადი ზოლით
     const embed = new EmbedBuilder()
-      .setDescription(`**${text}**`) // ვამატებთ ** სიმბოლოებს ტექსტის გარშემო გამუქებისთვის
+      .setDescription(text)
       .setColor(colorInput);
     
     // გაგზავნეთ embed
